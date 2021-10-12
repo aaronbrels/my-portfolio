42d7f167c5e1c06036c31e27a4780c1acc3f8e7b
Aaron Brelsford <aaronbrels@gmail.com>
2021-10-12 16:07:51 -0400

    First commit

---
+++ main.js
@@ -0,0 +1,11 @@
+// only allow certain view to show based on what button we click
+
+document.getElementById("home").addEventListener("click", toggleDisplay)
+document.getElementById("about").addEventListener("click", toggleDisplay)
+document.getElementById("portolio").addEventListener("click", toggleDisplay)
+document.getElementById("contact").addEventListener("click", toggleDisplay)
+
+function toggleDisplay(event) {
+
+
+}
